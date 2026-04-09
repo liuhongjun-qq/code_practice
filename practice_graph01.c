aee7f5c067b075edf2e1707c56f46ee49f9a4dbc
chao310 <1104201452@qq.com>
2026-04-09 14:32:34 +0800

    Add files via upload

---
+++ practice_graph01.c
@@ -0,0 +1,71 @@
+//用邻接矩阵的表示方法创建无向图
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAXNUM 100
+#define INF 65535
+
+typedef int VertexType;
+typedef int EdgeType;
+
+typedef struct {
+    VertexType vexs[MAXNUM];
+    EdgeType arcs[MAXNUM][MAXNUM];
+    int vexNum, arcNum;
+}Graph;
+
+//创建无向网（邻接矩阵）
+void createMGraph(Graph* G) {
+    int i, j, k;
+    int w;
+
+    printf("输入顶点数和边数：");
+    scanf("%d %d", &G->vexNum, &G->arcNum);
+
+    //输入顶点
+    printf("输入这%d个顶点的值：", G->vexNum);
+    for (i = 0; i < G->vexNum; i++) {
+        scanf("%d", &G->vexs[i]);
+    }
+
+    //初始化邻接矩阵
+    for (i = 0; i < G->vexNum; i++) {
+        for (j = 0; j < G->vexNum; j++) {
+            G->arcs[i][j] = INF;
+        }
+    }
+
+    //输入边
+    printf("\n输入各条边的顶点及权值：\n");
+    for (k = 0; k < G->arcNum; k++) {
+        printf("第%d条边：", k + 1);
+        scanf("%d %d %d", &i, &j, &w);
+        G->arcs[i][j] = w;
+        G->arcs[j][i] = w;
+    }
+}
+
+//输出邻接矩阵
+void printMGraph(Graph* G) {
+    int i, j;
+    printf("邻接矩阵为：\n");
+    for (i = 0; i < G->vexNum; i++) {
+        for (j = 0; j < G->vexNum; j++) {
+            if (G->arcs[i][j] == INF) {
+                printf("%-6s", "INF");
+            }
+            else {
+                printf("%-6d", G->arcs[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    Graph G;
+    createMGraph(&G);
+    printMGraph(&G);
+    return 0;
+}
