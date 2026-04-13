1aec1b3fde91b34ea92d40c37780cbb76a51d030
chao310 <1104201452@qq.com>
2026-04-13 14:28:21 +0800

    Add files via upload

---
+++ practice_graph06.c
@@ -0,0 +1,213 @@
+//设计算法实现求图G中距离顶点v的最短路径长度最大的一个顶点
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAXNUM 100
+#define INF 65535
+
+typedef int VertexType;
+typedef int EdgeType;
+
+typedef struct {
+    VertexType vexs[MAXNUM];
+    EdgeType arcs[MAXNUM][MAXNUM];
+    int vexNum, arcNum;
+}Graph;
+
+//创建无向网（邻接矩阵）
+void createMGraph(Graph* G) {
+    int i, j, k;
+    int w;
+
+    printf("输入顶点数和边数：");
+    scanf("%d %d", &G->vexNum, &G->arcNum);
+
+    //输入顶点
+    printf("输入这%d个顶点的值：", G->vexNum);
+    for (i = 0; i < G->vexNum; i++) {
+        scanf("%d", &G->vexs[i]);
+    }
+
+    //初始化邻接矩阵
+    for (i = 0; i < G->vexNum; i++) {
+        for (j = 0; j < G->vexNum; j++) {
+            G->arcs[i][j] = INF;
+        }
+    }
+
+    //输入边
+    printf("\n输入各条边的顶点及权值：\n");
+    for (k = 0; k < G->arcNum; k++) {
+        printf("第%d条边：", k + 1);
+        scanf("%d %d %d", &i, &j, &w);
+        G->arcs[i][j] = w;
+        G->arcs[j][i] = w;
+    }
+}
+
+//输出邻接矩阵
+void printMGraph(Graph* G) {
+    int i, j;
+    printf("邻接矩阵为：\n");
+    for (i = 0; i < G->vexNum; i++) {
+        for (j = 0; j < G->vexNum; j++) {
+            if (G->arcs[i][j] == INF) {
+                printf("%-6s", "INF");
+            }
+            else {
+                printf("%-6d", G->arcs[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+//增加一个顶点
+void InsertVex(Graph* G, VertexType v) {
+    G->vexs[G->vexNum] = v;
+    for (int i = 0; i <= G->vexNum; i++) {
+        G->arcs[G->vexNum][i] = INF;
+        G->arcs[i][G->vexNum] = INF;
+    }
+    G->vexNum++;
+}
+
+//删除一个顶点
+void DeleteVex(Graph* G, int pos) {
+    int i, j;
+
+    for (i = pos; i < G->vexNum - 1; i++) {
+        G->vexs[i] = G->vexs[i + 1];
+    }
+
+    //删除行
+    for (i = pos; i < G->vexNum - 1; i++) {
+        for (j = 0; j < G->vexNum; j++) {
+            G->arcs[i][j] = G->arcs[i + 1][j];
+        }
+    }
+
+    //删除列
+    for (j = pos; j < G->vexNum - 1; j++) {
+        for (i = 0; i < G->vexNum - 1; i++) {
+            G->arcs[i][j] = G->arcs[i][j + 1];
+        }
+    }
+    printf("顶点%d已被删除。\n", G->vexs[pos]);
+    G->vexNum--;
+}
+
+//增加一条边
+void InsertArc(Graph* G, int i, int j, int w) {
+    G->arcs[i][j] = w;
+    G->arcs[j][i] = w;
+    G->arcNum++;
+    printf("边(%d,%d)添加成功。\n", i, j);
+}
+
+//删除一条边
+void DeleteArc(Graph* G, int i, int j) {
+    G->arcs[i][j] = INF;
+    G->arcs[j][i] = INF;
+    G->arcNum--;
+    printf("边(%d,%d)删除成功。\n", i, j);
+}
+
+//深度优先遍历（递归）
+int visited[MAXNUM];
+
+void DFS(Graph* G, int v) {
+    printf("%d ", G->vexs[v]);
+    visited[v] = 1;
+    for (int i = 0; i < G->vexNum; i++) {
+        if (G->arcs[v][i] != INF && visited[i] == 0) {
+            DFS(G, i);
+        }
+    }
+}
+
+void DFSTraverse(Graph* G) {
+    int v;
+    for (v = 0; v < G->vexNum; v++) {
+        visited[v] = 0;
+    }
+    printf("\n深度优先遍历序列：");
+    DFS(G, 0);
+}
+
+//深度优先遍历（非递归）
+void DFS_NonRecursive(Graph* G, int start) {
+    int stack[MAXNUM];
+    int top = -1;
+    int i, v;
+    for (i = 0; i < G->vexNum; i++) {
+        visited[i] = 0;
+    }
+    printf("\n深度优先遍历序列：");
+    stack[++top] = start;
+    while (top != -1) {
+        v = stack[top--];
+        if (visited[v] == 0) {
+            printf("%d ", G->vexs[v]);
+            visited[v] = 1;
+        }
+        for (i = G->vexNum - 1; i >= 0; i--) {
+            if (G->arcs[v][i] != INF && visited[i] == 0) {
+                stack[++top] = i;
+            }
+        }
+    }
+}
+
+//找距离顶点v最远的顶点
+void FindFarthestVex(Graph* G, int v) {
+    int distance[MAXNUM];
+    int i, j, k;
+    int maxDist, maxVertex;
+
+    //初始化距离
+    for (i = 0; i < G->vexNum; i++) {
+        distance[i] = G->arcs[v][i];
+    }
+    distance[v] = 0;
+    visited[v] = 1;
+
+    //求最短路径
+    for (i = 1; i < G->vexNum; i++) {
+        int min = INF;
+        for (j = 0; j < G->vexNum; j++) {
+            if (visited[j] == 0 && distance[j] < min) {
+                min = distance[j];
+                k = j;
+            }
+        }
+        visited[k] = 1;
+
+        for (j = 0; j < G->vexNum; j++) {
+            if (visited[j] == 0 && G->arcs[k][j] != INF && distance[k] + G->arcs[k][j] < distance[j]) {
+                distance[j] = distance[k] + G->arcs[k][j];
+            }
+        }
+    }
+
+    //找出距离最大的顶点
+    maxDist = -1;
+    maxVertex = v;
+    for (i = 0; i < G->vexNum; i++) {
+        if (distance[i] > maxDist && distance[i] != INF) {
+            maxDist = distance[i];
+            maxVertex = i;
+        }
+    }
+    printf("\n距离顶点%d最远的顶点是：%d\n", v, maxVertex);
+    printf("最短路径长度为：%d\n", maxDist);
+}
+
+int main() {
+    Graph G;
+    createMGraph(&G);
+    printMGraph(&G);
+    FindFarthestVex(&G, 0);
+    return 0;
+}
---
+++ practice_graph07.c
@@ -0,0 +1,159 @@
+//设计算法实现基于深度优先的策略判别是否存在vi到vj的路径
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAXNUM 100
+#define INF 65535
+
+typedef int VertexType;
+typedef int EdgeType;
+
+typedef struct {
+    VertexType vexs[MAXNUM];
+    EdgeType arcs[MAXNUM][MAXNUM];
+    int vexNum, arcNum;
+}Graph;
+
+//创建无向网（邻接矩阵）
+void createMGraph(Graph* G) {
+    int i, j, k;
+    int w;
+
+    printf("输入顶点数和边数：");
+    scanf("%d %d", &G->vexNum, &G->arcNum);
+
+    //输入顶点
+    printf("输入这%d个顶点的值：", G->vexNum);
+    for (i = 0; i < G->vexNum; i++) {
+        scanf("%d", &G->vexs[i]);
+    }
+
+    //初始化邻接矩阵
+    for (i = 0; i < G->vexNum; i++) {
+        for (j = 0; j < G->vexNum; j++) {
+            G->arcs[i][j] = INF;
+        }
+    }
+
+    //输入边
+    printf("\n输入各条边的顶点及权值：\n");
+    for (k = 0; k < G->arcNum; k++) {
+        printf("第%d条边：", k + 1);
+        scanf("%d %d %d", &i, &j, &w);
+        G->arcs[i][j] = w;
+        G->arcs[j][i] = w;
+    }
+}
+
+//输出邻接矩阵
+void printMGraph(Graph* G) {
+    int i, j;
+    printf("邻接矩阵为：\n");
+    for (i = 0; i < G->vexNum; i++) {
+        for (j = 0; j < G->vexNum; j++) {
+            if (G->arcs[i][j] == INF) {
+                printf("%-6s", "INF");
+            }
+            else {
+                printf("%-6d", G->arcs[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+//增加一个顶点
+void InsertVex(Graph* G, VertexType v) {
+    G->vexs[G->vexNum] = v;
+    for (int i = 0; i <= G->vexNum; i++) {
+        G->arcs[G->vexNum][i] = INF;
+        G->arcs[i][G->vexNum] = INF;
+    }
+    G->vexNum++;
+}
+
+//删除一个顶点
+void DeleteVex(Graph* G, int pos) {
+    int i, j;
+
+    for (i = pos; i < G->vexNum - 1; i++) {
+        G->vexs[i] = G->vexs[i + 1];
+    }
+
+    //删除行
+    for (i = pos; i < G->vexNum - 1; i++) {
+        for (j = 0; j < G->vexNum; j++) {
+            G->arcs[i][j] = G->arcs[i + 1][j];
+        }
+    }
+
+    //删除列
+    for (j = pos; j < G->vexNum - 1; j++) {
+        for (i = 0; i < G->vexNum - 1; i++) {
+            G->arcs[i][j] = G->arcs[i][j + 1];
+        }
+    }
+    printf("顶点%d已被删除。\n", G->vexs[pos]);
+    G->vexNum--;
+}
+
+//增加一条边
+void InsertArc(Graph* G, int i, int j, int w) {
+    G->arcs[i][j] = w;
+    G->arcs[j][i] = w;
+    G->arcNum++;
+    printf("边(%d,%d)添加成功。\n", i, j);
+}
+
+//删除一条边
+void DeleteArc(Graph* G, int i, int j) {
+    G->arcs[i][j] = INF;
+    G->arcs[j][i] = INF;
+    G->arcNum--;
+    printf("边(%d,%d)删除成功。\n", i, j);
+}
+
+//深度优先搜索
+int visited[MAXNUM];
+
+void DFS(Graph* G, int v, int target, int* found) {
+    visited[v] = 1; 
+    if (v == target) {
+        *found = 1;
+        return;
+    }
+
+    for (int i = 0; i < G->vexNum; i++) {
+        if (G->arcs[v][i] != INF && !visited[i]) { 
+            DFS(G, i, target, found);
+        }
+    }
+}
+
+//判断是否存在从vi到vj的路径
+int IsExist(Graph* G, int vi, int vj) {
+    int found = 0;
+    for (int i = 0; i < G->vexNum; i++) {
+        visited[i] = 0; 
+    }
+    DFS(G, vi, vj, &found); 
+    return found;
+}
+
+int main() {
+    Graph G;
+    createMGraph(&G);
+    printMGraph(&G);
+    int vi, vj;
+    printf("输入vi和vj：");
+    scanf("%d %d", &vi, &vj);
+
+    if (IsExist(&G, vi, vj) == 1) {
+        printf("YES!\n", vi, vj);
+    }
+    else {
+        printf("NO!\n", vi, vj);
+    }
+    return 0;
+}
